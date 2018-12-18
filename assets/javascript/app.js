f7ebb4b1243bca7623f288cb917c7a2afb7d87bd
Toad Sage <kylenewins@gmail.com>
2018-12-18 14:48:54 -0500

    working app, might add later

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,21 @@ $(document).ready(function(){
     console.log("javascript linked")
 
     var database = firebase.database();
+    var currentTime = moment().format("HH:mm:ss")
+    var timerInt
+    $("#time-display").text(currentTime)
+    
+    function runTime() {
+        clearInterval(timerInt);
+        timerInt = setInterval(displayTime, 1000);
+      }
+    
+    function displayTime(){
+        currentTime = moment().format("HH:mm:ss")
+        $("#time-display").text(currentTime)
+    }
+
+    runTime();
 
 //on click event for submit button
     $("#sub-button").on("click", function(event){
@@ -22,27 +37,46 @@ $(document).ready(function(){
                 Frequency: frequency
             })
 
-        //pull the values back into the table
         $("#table-body").empty()
-        
+
+        //pull the values back into the table
         database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
+        //creates a new row with data whenever there is a new child in the firebase (ie submitted)
             var newRow = $("<tr>")
 
             var nameData = $("<td>")
             var destData = $("<td>")
-            var ftData = $("<td>")
             var freqData = $("<td>")
+            var naData = $("<td>")
+            var maData = $("<td>")
+        
+        //train math to figure out next arrival and minutes away from First Time and Frequency inputs
+        var tFrequency = snapshot.val().Frequency
+        var firstTime = snapshot.val().FirstTime
+        
+        var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes")
+        var tRemainder = diffTime % tFrequency;
+        var tMinutesTillTrain = tFrequency - tRemainder;
+        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        var nextTrainConvert = moment(nextTrain).format("HH:mm")
 
+        //setting the text to the corresponding values
             nameData.text(snapshot.val().TrainName)
             destData.text(snapshot.val().Destination)
-            ftData.text(snapshot.val().FirstTime)
             freqData.text(snapshot.val().Frequency)
+            naData.text(nextTrainConvert)
+            maData.text(tMinutesTillTrain)
 
+
+        //appending the data cells to the newRow
             newRow.append(nameData)
             newRow.append(destData)
-            newRow.append(ftData)
             newRow.append(freqData)
-
+            newRow.append(naData)
+            newRow.append(maData)
+        
+        //appending the new row to the table body
             $("#table-body").append(newRow)
 
         })//child added close
@@ -74,4 +108,4 @@ $(document).ready(function(){
 
 
 
-})
+})//document ready close
